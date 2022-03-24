a721ff7c60edca10ae2f6d944f818c38ff321b24
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-24 12:53:33 -0500

    Initialize User Schema and model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const emailRegex = require('../utils/emailRegex');
+
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please provide a name'],
+		minLength: 3,
+		maxLength: 50,
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide an email'],
+		match: [emailRegex, 'Please provide a valid email'],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide a password'],
+		minLength: 6,
+		maxLength: 12,
+	},
+});
+
+module.exports = mongoose.model('User', UserSchema);
