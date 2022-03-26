2a511e6bb6a5d806a539b50a7b7856c639a616a8
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-25 20:52:21 -0500

    Add security packages

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// Express app
 const express = require('express');
 const app = express();
 
@@ -14,9 +23,21 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100,
+	})
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
+app.get('/', (req, res) => {
+	res.send('Jobs Api');
+});
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
